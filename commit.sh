b2a5ddf3bf22d9b3791d85f67a375f861a8b349d
Sam Scott <samscott@mcmaster.ca>
2024-03-19 14:59:53 -0400

    Repetition operator added

--- commit.sh
+++ commit.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # TODO - get a tag as a parameter and push it as well
 
-$message = ""
+message=""
 
-while getopts u:a:f: flag
+while getopts m: flag
 do
     case "${flag}" in
         m) message=${OPTARG};;
     esac
 done
 
+echo $message
+
 git add *.c *.h *.sh makefile
-git commit -m message
+git commit -m "$message"
 git push origin
 git status
