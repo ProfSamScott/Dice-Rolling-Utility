1d8c232c04f6e80b8783604e77909276fa62d2d5
Sam Scott <samscott@mcmaster.ca>
2024-03-19 19:10:38 -0400

    added tag to commit script

--- commit.sh
+++ commit.sh
@@ -2,11 +2,13 @@
 # TODO - get a tag as a parameter and push it as well
 
 message=""
+tag=""
 
-while getopts m: flag
+while getopts m:t: flag
 do
     case "${flag}" in
         m) message=${OPTARG};;
+        t) tag=${OPTARG};;
     esac
 done
 
@@ -21,3 +23,7 @@ else
 fi
 git push origin
 git status
+if [$tag != ""]
+then
+    git push $tag
+fi
